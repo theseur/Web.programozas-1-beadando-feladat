496e4c337141591f343587c087f02c62280b5b1c
Farkas Tibor <32579497+theseur@users.noreply.github.com>
2022-04-02 21:02:36 +0200

    sikeres feltoltes

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
+
 <?php
 include_once("connect.php");
 if(!isset($_GET["page"]))
 {
     echo 'fÖoldal';
+}
+else
+{
+    if(($_GET["page"]=="kapcsolat"))
+    {
+        include_once("kapcsolat.html");
+    }
+    else if ($_GET["page"]=="kapcsolatkuldes")
+    {
+        include_once("php/kapcsolat.php");
+    }
+
 }
 ?>
---
+++ php/kapcsolat.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+<?php
+	//szerver oldali ellenőrzés példa
+
+	if(!isset($_POST['nev']) || strlen($_POST['nev']) < 5)
+	{
+		exit("Hibás név: ".$_POST['nev']);
+	}
+
+	$re = '/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/';
+	if(!isset($_POST['email']) || !preg_match($re,$_POST['email']))
+	{
+		exit("Hibás email: ".$_POST['email']);
+	}
+
+	if(!isset($_POST['szoveg']) || empty($_POST['szoveg']))
+	{
+		exit("Hibás szöveg: ".$_POST['szoveg']);
+	}
+
+	echo "Kapott értékek: ";
+	echo "<pre>";
+	var_dump($_POST);
+	echo "</pre>";
+	$sth = $pdo->prepare("INSERT INTO `beadando-maltai`.`uzenet` (`id`, `nev`, `email`, `uzenet`, `datum`) VALUES (NULL, ?, ?, ?, CURRENT_TIMESTAMP);");
+	$sth->execute(array($_POST['nev'], $_POST['email'],$_POST['szoveg']));
+?>
+	</body>
+</html>
