0d4c16e2332f83bd237d6284e56c9d7ec46b6c88
Farkas Tibor <32579497+theseur@users.noreply.github.com>
2022-04-09 15:10:30 +0200

    kepfeltolt

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ include_once("connect.php");
 if(!isset($_GET["page"]))
 {
     echo 'fÖoldal';
+     include("aside.html"); 
 }
 else
 {
@@ -14,6 +15,30 @@ else
     else if ($_GET["page"]=="kapcsolatkuldes")
     {
         include_once("php/kapcsolat.php");
+    }
+    else if($_GET["page"]=="uzenetkiolvasas")
+    {
+        include_once("uzenetkiolvasas.php");
+
+    }
+    else if($_GET["page"]=="video")
+    {
+        include_once("VideoTest.php");
+    }
+    else if($_GET["page"]=="galeria")
+    {
+        include_once("php/gallery.php");
+
+    }
+    else if($_GET["page"]=="feltotles")
+    {
+        include_once("php/feltolt.php");
+
+    }
+    else if($_GET["page"]=="mao")
+    {
+        include_once("php/terkep.php");
+
     }
 
 }
