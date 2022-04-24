3a139d4d770ddd867fe1406c6b9752ac56d8f60f
hegedd <hegedd@users.noreply.github.com>
2022-04-24 18:26:59 +0200

    Update beadando-maltai.sql

--- beadando-maltai.sql
+++ beadando-maltai.sql
@@ -1,26 +1,41 @@
-CREATE DATABASE `beadando-maltai`
-CHARACTER SET utf8 COLLATE utf8_general_ci;
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2022. Ápr 24. 18:22
+-- Kiszolgáló verziója: 10.4.18-MariaDB
+-- PHP verzió: 8.0.3
 
-USE `beadando-maltai`;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Adatbázis: `beadando-maltai`
 --
+CREATE DATABASE IF NOT EXISTS `beadando-maltai` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci;
+USE `beadando-maltai`;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet: `felhasznalok`
+-- Tábla szerkezet ehhez a táblához `felhasznalok`
 --
 
-CREATE TABLE IF NOT EXISTS `felhasznalok` (
-  `id` int(11) NOT NULL auto_increment,
-  `csaladi_nev` varchar(45) collate utf8_hungarian_ci NOT NULL,
-  `uto_nev` varchar(45) collate utf8_hungarian_ci NOT NULL,
-  `bejelentkezes` varchar(45) collate utf8_hungarian_ci NOT NULL,
-  `jelszo` varchar(45) collate utf8_hungarian_ci NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci AUTO_INCREMENT=20 ;
+CREATE TABLE `felhasznalok` (
+  `id` int(11) NOT NULL,
+  `csaladi_nev` varchar(45) COLLATE utf8_hungarian_ci NOT NULL DEFAULT '',
+  `uto_nev` varchar(45) COLLATE utf8_hungarian_ci NOT NULL DEFAULT '',
+  `bejelentkezes` varchar(45) COLLATE utf8_hungarian_ci NOT NULL DEFAULT '',
+  `jelszo` varchar(45) COLLATE utf8_hungarian_ci NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `felhasznalok`
@@ -38,30 +53,66 @@ INSERT INTO `felhasznalok` (`id`, `csaladi_nev`, `uto_nev`, `bejelentkezes`, `je
 (9, 'Családi_9', 'Utónév_9', 'Login9', 'c0298f7d314ecbc5651da5679a0a240833a88238'),
 (10, 'Családi_10', 'Utónév_10', 'Login10', 'a477427c183664b57f977661ac3167b64823f366'),
 (11, 'Családi_11', 'Utónév_11', 'Login11', '6c7e2151d33968a02198f7e2073b1d7b9ee65ed9'),
-(12, 'Családi_12', 'Utónév_12', 'Login12', '0722b3651be10eeb8df39cced958b74a98d18ce3'),
+(12, 'Családi_12', 'Utónév_12', 'Login12', '0722b3651be10eeb8df39cced958b74a98d18ce3');
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet: `uzenet`
+-- Tábla szerkezet ehhez a táblához `uzenet`
 --
 
-CREATE TABLE IF NOT EXISTS `uzenet` (
-  `id` int(11) NOT NULL auto_increment,
-  `nev` varchar(45) character set utf8 collate utf8_hungarian_ci NOT NULL default 'Vendég',
-  `email` varchar(100) character set utf8 collate utf8_hungarian_ci NOT NULL,
-  `uzenet` text character set utf8 collate utf8_hungarian_ci NOT NULL,
-  `datum` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
+CREATE TABLE `uzenet` (
+  `id` int(11) NOT NULL,
+  `nev` varchar(45) CHARACTER SET utf8 COLLATE utf8_hungarian_ci NOT NULL DEFAULT 'Vendég',
+  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_hungarian_ci NOT NULL DEFAULT '',
+  `uzenet` text CHARACTER SET utf8 COLLATE utf8_hungarian_ci NOT NULL DEFAULT '',
+  `datum` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- A tábla adatainak kiíratása `uzenet`
 --
 
 INSERT INTO `uzenet` (`id`, `nev`, `email`, `uzenet`, `datum`) VALUES
-(1, 'Vendég', 'mano@mano.hu', 'afsddfsa', '2022-04-23 19:53:21'),
-(2, 'Vendég', 'mano@mano.hu', 'afsddfsa', '2022-04-23 19:54:05'),
-(3, 'Vendég', 'hekale@freemail.hu', 'fffff', '2022-04-23 19:54:23'),
-(4, 'Vendég', 'tredet@gtr.hu', 'hhhh', '2022-04-23 20:11:31'),
-(5, 'Családi_1 Utónév_1', 'jztr@freemail.hu', 'ffffff', '2022-04-23 20:14:27');
+(1, 'Vendég', 'mano@mano.hu', 'afsddfsa', '2022-04-23 17:53:21'),
+(2, 'Vendég', 'mano@mano.hu', 'afsddfsa', '2022-04-23 17:54:05'),
+(3, 'Vendég', 'hekale@freemail.hu', 'fffff', '2022-04-23 17:54:23'),
+(4, 'Vendég', 'tredet@gtr.hu', 'hhhh', '2022-04-23 18:11:31'),
+(5, 'Családi_1 Utónév_1', 'jztr@freemail.hu', 'ffffff', '2022-04-23 18:14:27');
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `uzenet`
+--
+ALTER TABLE `uzenet`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT a táblához `uzenet`
+--
+ALTER TABLE `uzenet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
